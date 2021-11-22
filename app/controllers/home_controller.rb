1a9f9be5651b9fd67dd3ba40fc2aecc29994e796
Anibal <cubanibal@hotmail.com>
2021-11-22 16:09:26 +0100

    Estaciones

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+  
+  end
+  def estaciones
+    require 'net/http'
+    require 'json'
+
+
+    @url = URI("https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdWJhbmliYWxAaG90bWFpbC5jb20iLCJqdGkiOiIyYTdkNWE1YS00YTM0LTRkYjEtOWUyMS1hZTBmMTZmMGM1ZjciLCJpc3MiOiJBRU1FVCIsImlhdCI6MTYyNjE5MTgyOSwidXNlcklkIjoiMmE3ZDVhNWEtNGEzNC00ZGIxLTllMjEtYWUwZjE2ZjBjNWY3Iiwicm9sZSI6IiJ9.WuQ4ezid3wNtsOhdC8zGLG5rJPbAmowBVJuNrFeXL_8")
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @valores = JSON.parse(@response)
+    @tiempo = @valores["datos"]
+    @uri_tiempo = URI(@tiempo)
+    @response2 = Net::HTTP.get(@uri_tiempo)
+    @valores2 = JSON.parse(@response2)
+    
+
+
   end
 end
